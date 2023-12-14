4c308b0d1b388a3d664b26d81c70011afbd2c7e9
OliverKovacs <oliver.kovacs.dev@gmail.com>
2023-12-15 00:13:13 +0100

    Day 13

---
+++ day13/part1.js
@@ -0,0 +1,31 @@
+const equal = (a, b) => a
+    .slice(0, b.length)
+    .every((e, i) => e === b[i])
+
+const find = (arr, rarr, s) => {
+    for (let i = 1; i < arr.length; i++) {
+        if (equal(arr.slice(i), rarr.slice(arr.length - i))) return i * s;
+    }
+    return 0;
+}
+
+const reflection = pat => {
+    const rows = pat
+        .map(e => e.split("").reduce((p, c, i) => p | ((c === "#") << i), 0));
+    const cols = pat[0]
+        .split("")
+        .map((_, i) => pat.reduce((p, c, j) => p | ((c[i] === "#") << j), 0));
+    const rrows = rows.reduce((p, c) => [ c, ...p ], []);
+    const rcols = cols.reduce((p, c) => [ c, ...p ], []);
+    return find(rows, rrows, 100) + find(cols, rcols, 1);
+};
+
+const part1 = file => require("node:fs")
+    .readFileSync(file, "utf-8")
+    .slice(0, -1)
+    .split("\n\n")
+    .map(pat => pat.split("\n"))
+    .map(reflection)
+    .reduce((p, c) => p + c);
+
+console.log(part1("node"));
